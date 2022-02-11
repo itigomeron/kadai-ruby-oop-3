b9978bb880db79a3800da47d9ac53f1abe0b36d8
Ubuntu <ubuntu@ip-172-31-30-53.ap-northeast-1.compute.internal>
2022-02-11 06:53:22 +0000

    add main.rb

---
+++ main.rb
@@ -0,0 +1,12 @@
+require './human'
+
+# インスタンスの作成
+tanaka = Human.new("tanaka",25,"電車")
+suzuki = Human.new("suzuki",30,"野球")
+sato   = Human.new("sato",20,"映画")
+tanaka.say
+tanaka.think
+suzuki.say
+suzuki.think
+sato.say
+sato.think
